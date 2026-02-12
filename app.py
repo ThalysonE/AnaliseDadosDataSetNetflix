26ea29e4efb2e381c905cd315d7d6210baa0a23e
ThalysonE <thalysonelione8@gmail.com>
2026-02-12 16:48:04 -0300

    Criando grafico de tiulos por pais e de quantidade de titulos adicionados em cada mes

--- app.py
+++ app.py
@@ -3,21 +3,21 @@ import pandas
 import datetime
 import matplotlib.pyplot as plt
 #criando o dataframe
-df = pandas.read_csv("netflix_titles.csv", index_col="show_id")
+df = pandas.read_csv("netflix_titles.csv")
 
 # 1.1 - Limpeza e preparação dos dados
-df.dropna(inplace=True) # removeno linha com valores NA
+df.dropna(subset=['title', 'date_added'], inplace=True) # removeno linha com valores NA
 
 df["date_added"] = pandas.to_datetime(df["date_added"].str.strip() ,errors="coerce") # convertendo a coluna date_added para o formato datetime
 
 
 # 1.2 - Tratando colunas com múltiplos valores separados por vírgula
 df["country"] = df["country"].str.split(", ")
-df["country"].explode() # separando os países em linhas diferentes
+df = df.explode("country").reset_index(drop=True) # separando os países em linhas diferentes
 df["listed_in"] = df["listed_in"].str.split(", ")
-df["listed_in"].explode() # separando os gêneros em linhas diferentes
+df = df.explode("listed_in").reset_index(drop=True) # separando os gêneros em linhas diferentes
 df["cast"] = df["cast"].str.split(", ")
-df["cast"].explode() # separando os atores em linhas diferentes
+df = df.explode("cast").reset_index(drop=True) # separando os atores em linhas diferentes
 
 # 1.3 - Criando colunas auxiliares
 df["year_added"] = df["date_added"].dt.year # criando uma nova coluna com o ano de adição
@@ -28,7 +28,7 @@ df["release_on_netlix_diference"] = df["year_added"] - df["release_year"] # cria
 #2.0 - Análise exploratória dos dados
 
 #2.1 - Visualização da quantidade de filmes e séries na Netflix
-novoDf = df["type"].groupby(df["type"]).size().reset_index(name="count")
+novoDf = df.groupby("type")["show_id"].nunique().reset_index(name="count")
 novoDf.plot(
     x = "type",
     y = "count",
@@ -49,14 +49,14 @@ plt.tight_layout()
 #2.2 - Visualização da quantidade de lançamentos por ano
 
 #2.2.1 - Identificação de picos de crescimento(titulos adicionados por ano)
-titles_by_year = df.groupby("year_added").size().sort_index()
+titles_by_year = df.groupby("year_added")["show_id"].nunique().sort_index()
 diference = titles_by_year.diff()
 peak_year = diference.idxmax()
 peak_value = diference.max()
-print(f"O pico de crescimento ocorreu em {peak_year} com um aumento de {peak_value} títulos em relação ao ano anterior.")   
+# print(f"O pico de crescimento ocorreu em {peak_year} com um aumento de {peak_value} títulos em relação ao ano anterior.")   
 
 #2.2.2 - Visualização do crescimento de títulos por ano
-novoDf2 = df["year_added"].groupby(df["year_added"]).size().sort_index().reset_index(name="count")
+novoDf2 = df.groupby("year_added")["show_id"].nunique().sort_index().reset_index(name="count")
 
 novoDf2.plot(
     x = "year_added",
@@ -77,7 +77,7 @@ plt.axvline(peak_year, linestyle= "--")
 plt.show()
 
 #2.3 - Visualização da quantidade de títulos adicionados por mês
-titles_add_by_month = df.groupby("month_added").size().reset_index(name="count")
+titles_add_by_month = df.groupby("month_added")["show_id"].nunique().reset_index(name="count")
 titles_add_by_month["month_added"]= ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 titles_add_by_month.plot(
@@ -92,5 +92,23 @@ plt.xlabel("Mês de adição", fontsize= 12, fontweight= "bold")
 plt.ylabel("Quantidade de títulos adicionados", fontsize= 12, fontweight= "bold")
 plt.tight_layout()
 plt.show() 
+
+#2.4 - Top 10 países com mais títulos na Netflix
+titles_by_contry = df.groupby("country")["show_id"].nunique().reset_index(name="count").sort_values(by="count", ascending=False)
+
+titles_by_contry.head(10).plot(
+    x= "country",
+    y="count",
+    kind="bar",
+    title="Top 10 países com mais títulos na Netflix",
+    color = 'cyan',
+    legend=False
+)
+
+plt.xlabel("País", fontsize= 12, fontweight= "bold")
+plt.ylabel("Quantidade de títulos", fontsize=12, fontweight="bold")
+plt.xticks(rotation=45, ha="right")
+plt.tight_layout()
+plt.show()
 
 
